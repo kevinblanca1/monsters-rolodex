5d41dfe0a093d82df1cbaa2dfacee7d392d18169
Kevin Josef Blanca <blanca@mclinica.com>
2020-05-24 18:45:31 +0800

    Updates

---
+++ precache-manifest.88ad4cfd824ea60faf291706e722a44b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d7caa44758edc0086bdff7902b0b6f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3c3f38dce32dc2b9ab53",
+    "url": "/monsters-rolodex/static/css/main.80ee5c06.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c3f38dce32dc2b9ab53",
+    "url": "/monsters-rolodex/static/js/main.713c4e35.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
